04ae3fa0a0502aa5b4ee8fceba15501e72575b77
Bill Tiffany <daganlegalservice@gmail.com>
2021-10-26 11:33:24 -0600

    adding new files

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const path = require('path')
 
 const app = express()
 
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar({
+    accessToken: 'ec44c98d9f6d448690fe8a967482e9f7',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully.')
 })
 
 const port = process.env.PORT || 4242
